529c39347e2d440b3fe4c9e7cea22aac1da38821
Aleksey Prokopenko <aleksey@incsw.in>
2015-06-12 18:45:23 +0200

    fixed router test and parameter name
    Signed-off-by: Aleksey Prokopenko <aleksey@incsw.in>

--- test/router.js
+++ test/router.js
@@ -7,52 +7,52 @@ describe('Router', function() {
 
     beforeEach(function() {
         router = new Router();
-        router.add('foo', new Route('GET', '/this/is/url/:parameter', function() {}));
+        router.add('foo', new Route('GET', '/', function() {}));
     });
     
     describe('#add', function() {
-        it('should throw error when keys are duplicates', function() {
-            assert.throws(function() {
-                router.add('foo', new Route('GET', '/this/is/url/:param', function() {}));
-            }, Error);
-        });
         it('should not throw error when all ok', function() {
             assert.doesNotThrow(function() {
-                router.add('bar', new Route('GET', '/this/is/url/:param', function() {}));
+                router.add('bar', new Route('GET', '/', function() {}));
+            }, Error);
+        });
+        it('should throw error when keys are duplicates', function() {
+            assert.throws(function() {
+                router.add('foo', new Route('GET', '/', function() {}));
             }, Error);
         });
     });
     
     describe('#match', function() {
-        it('should return null when the method incorrect or url not found', function() {
-            assert.equal(null, router.match('METHOD', '/this/is/url/parameter'));
-            assert.equal(null, router.match('GET', '/this/is/incorrect/url'));
-        });
         it('should return Object with route and parameters when url was found', function() {
-            var matched = router.match('GET', '/this/is/url/parameter');
+            var matched = router.match('GET', '/');
             
-            assert.equal('object', typeof matched);
-            assert.equal(true, matched.data instanceof Map);
-            assert.equal(true, matched.route instanceof Route);
+            assert.strictEqual('object', typeof matched);
+            assert.strictEqual(true, matched.data instanceof Map);
+            assert.strictEqual(true, matched.route instanceof Route);
+        });
+        it('should return null when the method incorrect or url not found', function() {
+            assert.strictEqual(null, router.match('METHOD', '/'));
+            assert.strictEqual(null, router.match('GET', '/foo'));
         });
     });
     
     describe('#route', function() {
         it('should return null when route not found', function() {
-            assert.equal(null, router.route('bar'));
+            assert.strictEqual(null, router.route('bar'));
         });
         it('should return Route when route found', function() {
-            assert.equal(true, router.route('foo') instanceof Route);
+            assert.strictEqual(true, router.route('foo') instanceof Route);
         });
     });
     
     describe('#getUrl', function() {
-        it('should return null when wout not found or not all parameters are set', function() {
-            assert.equal(null, router.getUrl());
-            assert.equal(null, router.getUrl('bar'));
-        });
         it('should return string when all ok', function() {
-            assert.equal('/this/is/url/parameter', router.getUrl('foo', new Map([['parameter', 'parameter']])));
+            assert.strictEqual('/', router.getUrl('foo'));
+        });
+        it('should return null when rout not found or not all parameters are set', function() {
+            assert.strictEqual(null, router.getUrl());
+            assert.strictEqual(null, router.getUrl('bar'));
         });
     });
 });
